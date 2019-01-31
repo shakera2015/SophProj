b8df786c1530c92183919e5c2fc8fd044a070137
bshuryan <brianshuryan@gmail.com>
2019-01-31 13:13:31 -0500

    First draft

---
+++ app/src/main/java/com/example/csi_app/FirstTimeUser.java
@@ -0,0 +1,15 @@
+package com.example.csi_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstTimeUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_time_user);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/csi_app/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.csi_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+    public void forgotPassword(View view) {
+
+        openForgotPassword();
+
+
+    }
+
+    public void newUser(View view){
+        openNewUser();
+    }
+
+    public void openForgotPassword(){
+
+        Intent intent = new Intent(this,ForgotPassword.class);
+        startActivity(intent);
+    }
+
+    public void openNewUser(){
+        Intent i2 = new Intent(this, FirstTimeUser.class);
+        startActivity(i2);
+    }
+}
